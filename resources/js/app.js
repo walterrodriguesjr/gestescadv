bb87618ebc21de1e42aa00c7227c5889e46588ef
Walter Rodrigues Junior <walterrjr.86@gmail.com>
2024-11-18 20:14:33 -0300

    finalizado. Foram inseridas as bibliotecas de: jquery, datatables, bootstrap ja tem. Foi inserido via comando de npm e depois colocadas em app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 import './bootstrap';
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+import 'datatables.net-dt/js/dataTables.dataTables';
+import 'datatables.net-dt/css/dataTables.dataTables.css';
